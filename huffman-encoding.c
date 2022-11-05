5dc00d63c8677d780c67743c8ebe66d8a35b0b79
vedanthv <vedanthvbaliga@gmail.com>
2022-11-05 10:21:43 +0530

    allocate heap to single character

--- huffman-encoding.c
+++ huffman-encoding.c
@@ -1,8 +1,6 @@
 // C Program for Huffman Encoding
-
 #include<stdio.h>
 #include<stdlib.h>
-
 #define MAX_TREE_H 100
 
 // Huffman Tree Node Structure
@@ -13,10 +11,21 @@ struct MinHeapNode{
 };
 
 // MinHeap -> Collection of Huffman Tree nodes
-
 struct MinHeap{
-    unsigned size;
-    unsigned capacity;
-    struct MinHeapNode **array;
+    unsigned size; // Current size of minHeap
+    unsigned capacity; //Capacity of MinHeap
+    struct MinHeapNode **array; // Array of pointers
 };
+
+// Utility funtion to allocate a heap to a single character
+struct MinHeapNode* newNode(int data, unsigned freq)
+{
+    struct MinHeapNode* temp =
+          (struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+    return temp;
+}
+
 
