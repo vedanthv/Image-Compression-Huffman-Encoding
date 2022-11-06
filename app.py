2ddc8508e31af48a3fbdbfa6fd3e67f247df3419
vedanthv <vedanthvbaliga@gmail.com>
2022-11-06 11:12:27 +0530

    app.py:decompress route

--- app.py
+++ app.py
@@ -43,6 +43,48 @@ def compress():
 		open('compressed.txt','w').writelines(dtbw)
 		return decompress()
 
+def decompress():
+	start=timer()
+	if request.method=='POST':
+		global s
+		d=[]
+		with open('compressed.txt','r') as f:
+			l=[]
+			f=f.read().split('\n')
+			f=f[:len(f)-1]
+			l,size=[i for i in f[:len(f)-1]],f[-1:][0]
+			for i in l:
+				for key,value in s.items():
+					if value==i:
+						d.append(key)
+			d=[str(i)+'\n' for i in d]
+			d.append(str(size))
+			open('decompressed.txt','w').writelines(d)
+		with open('decompressed.txt','r') as f:
+			l=list(f.read().split())
+			l1=[]
+			l2=[]
+			l3=[]
+			l1=[int(l[i]) for i in range(0,len(l)-2,3)]
+			l2=[int(l[i]) for i in range(1,len(l)-2,3)]
+			l3=[int(l[i]) for i in range(2,len(l)-2,3)]
+			res=list(zip(l1,l2,l3))
+			with open('final.txt','w') as file:
+				for i in res:
+					file.write(str(i)+'\n')
+				file.write(l[len(l)-2])
+				file.write(l[len(l)-1])
+    
+		txttoimg.txttoimg('final.txt')
+		response=make_response(send_from_directory('/D:/University/Image-Compression-Huffman Encoding','pic.jpeg'))
+		response.headers["Content-Disposition"]="attachment; filename=compressed_image.jpg"
+		end=timer()
+		duration=end-start
+		print("Duration in seconds:",duration)
+		return response
+
+
+
 @app.route('/about',methods=['GET','POST'])
 def about():
 	return render_template('about.html')
